41c77cebce5f14c4057117ef4bd65a35c23aa311
intro_java <karinap.pessoa@gmail.com>
2022-04-19 16:10:54 -0400

    Initial commit

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,47 @@
+// 1- pacote
+package intro;
+
+// 2 - Referência as bibliotecas
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Caracteristicas
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+
+        calcularAreaModoCurto();
+        calcularAreaModoExtenso();
+
+        }
+
+        public static void calcularAreaModoCurto(){
+            System.out.println("Cálculo de Areas Modo Curto");
+
+            // Calculo de área - Reduzido
+            int largura = 4;
+            int comprimento = 3;
+
+            System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                    + comprimento + "m a área é de " + largura * comprimento + "m² ");
+        }
+
+        public static void calcularAreaModoExtenso(){
+
+            System.out.println("Cálculo de Areas Modo Extenso");
+
+        // Calculo de área - Exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5; // largura recebe 4
+        comprimento = 6;
+
+        resultado = largura * comprimento;
+        System.out.println("Para a largura de " + largura + "m e o comprimento de "
+                + comprimento + "m a área é de " + resultado + "m² ");
+    }
+
+
+}
